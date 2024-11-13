9a3ff279efaf72369686d0d6945aa346e1a33ffc
delmas <azertyazerty1ze@gmail.com>
2024-11-13 10:02:09 +0000

    feature/ad/#1-password-reset

--- src/main/java/ci/digitalacademy/com/config/CloudinaryConfig.java
+++ src/main/java/ci/digitalacademy/com/config/CloudinaryConfig.java
@@ -2,16 +2,21 @@ package ci.digitalacademy.com.config;
 
 import com.cloudinary.Cloudinary;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${cloudinary.url}")
+    private String cloudinaryUrl;
 
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load();
-        Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+//        Dotenv dotenv = Dotenv.load();
+//        Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+//        String cloudinaryUrl = System.getenv("CLOUDINARY_URL");
+        Cloudinary cloudinary = new Cloudinary(cloudinaryUrl);
         cloudinary.config.secure = true;
         System.out.println(cloudinary.config.cloudName);
         return cloudinary;
